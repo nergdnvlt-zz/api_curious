0eacd910d82b9c50238692574a6597fe36f82227
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-10 13:28:57 -0600

    Additional testing.

--- spec/features/user_can_see_own_attributes_spec.rb
+++ spec/features/user_can_see_own_attributes_spec.rb
@@ -14,8 +14,30 @@ feature 'user can see a profile' do
       expect(current_path).to eq('/nergdnvlt')
 
       # I can see my profile pic, number of starred repo's, followers and who I'm following
+      expect(page).to have_css('.pic')
+
+      expect(page).to have_css('.name')
       expect(page).to have_content('Tyler Lundgren')
+
+      expect(page).to have_css('.username')
       expect(page).to have_content('nergdnvlt')
+
+      expect(page).to have_css('.email')
+      expect(page).to have_content('tylerlundgren@yahoo.com')
+
+      expect(page).to have_css('.location')
+      expect(page).to have_content('Boulder, CO')
+
+      expect(page).to have_css('.bio')
+
+      expect(page).to have_css('.followers')
+      expect(page).to have_content('1')
+
+      expect(page).to have_css('.following')
+      expect(page).to have_content('13')
+
+      expect(page).to have_css('.public_repos')
+      expect(page).to have_content('35')
     end
   end
 end
