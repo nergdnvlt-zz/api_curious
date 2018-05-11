511a7edb1aa7a6eaa6a7c65dda99c20d25d63f1f
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-11 08:36:38 -0600

    Fixed failing tests.

--- spec/features/user_can_see_own_repos_on_profile_spec.rb
+++ spec/features/user_can_see_own_repos_on_profile_spec.rb
@@ -4,12 +4,16 @@ require './spec/support/omniauth'
 feature 'user can their own repos' do
   scenario 'on their profile page' do
     VCR.use_cassette('features/user_can_see_own_repos_on_profile') do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit '/'
 
-      click_on 'Login With Github'
+      # click_on 'Login With Github'
       expect(current_path).to eq(root_path)
 
-      click_on 'Profile'
+      click_on 'Visit Your Profile Here'
+
       expect(current_path).to eq('/nergdnvlt')
 
       expect(page).to have_css('.repo', count: 6)
